303cdcb14817fa9994a428881291afe225c9663b
Segev Finer <segev@swimm.io>
2024-12-03 20:12:04 +0200

    Try to fix

--- build-windows.sh
+++ build-windows.sh
@@ -16,7 +16,7 @@ mkdir -p build/windows-x86_64
 pushd build/windows-x86_64
 
 echo "[*] Building ctags"
-tar xf ../../tars/$CTAGS_TAR
+7z x ../../tars/$CTAGS_TAR
 pushd $CTAGS_NAME
 ./autogen.sh
 ./configure --prefix=$TARGET_DIR --disable-external-sort --enable-static
--- download.sh
+++ download.sh
@@ -2,29 +2,28 @@ strip_tar_ext() {
     echo ${1/%.tar.*}
 }
 
-
 CTAGS_URL=https://github.com/universal-ctags/ctags/archive/refs/tags/p6.1.20241201.0.tar.gz
-CTAGS_TAR=p6.1.20241201.0.tar.gz
-CTAGS_NAME=$(strip_tar_ext $CTAGS_TAR)
+CTAGS_TAR=$(basename $CTAGS_URL)
+CTAGS_NAME=ctags-$(strip_tar_ext $CTAGS_TAR)
 
 JANSSON_URL=https://github.com/akheron/jansson/releases/download/v2.14/jansson-2.14.tar.bz2
-JANSSON_TAR=jansson-2.14.tar.bz2
+JANSSON_TAR=$(basename $JANSSON_URL)
 JANSSON_NAME=$(strip_tar_ext $JANSSON_TAR)
 
-PCRE2_URL=https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.42/pcre2-10.42.tar.bz2
-PCRE2_TAR=pcre2-10.42.tar.bz2
+PCRE2_URL=https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.bz2
+PCRE2_TAR=$(basename $PCRE2_URL)
 PCRE2_NAME=$(strip_tar_ext $PCRE2_TAR)
 
 # ICU4C_URL=https://github.com/unicode-org/icu/releases/download/release-73-2/icu4c-73_2-src.tgz
 # ICU4C_TAR=icu4c-73_2-src.tgz
 # ICU4C_NAME=icu
 
-LIBXML2_URL=https://download.gnome.org/sources/libxml2/2.11/libxml2-2.11.5.tar.xz
-LIBXML2_TAR=libxml2-2.11.5.tar.xz
+LIBXML2_URL=https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.5.tar.xz
+LIBXML2_TAR=$(basename $LIBXML2_URL)
 LIBXML2_NAME=$(strip_tar_ext $LIBXML2_TAR)
 
 LIBYAML_URL=http://pyyaml.org/download/libyaml/yaml-0.2.5.tar.gz
-LIBYAML_TAR=yaml-0.2.5.tar.gz
+LIBYAML_TAR=$(basename $LIBYAML_URL)
 LIBYAML_NAME=$(strip_tar_ext $LIBYAML_TAR)
 
 download_tar() {
