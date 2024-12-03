d5577037cd554b649655528f08932f1ba9caf216
Segev Finer <segev208@gmail.com>
2024-12-03 23:53:18 +0200

    Try to fix again

--- build-windows.sh
+++ build-windows.sh
@@ -16,7 +16,7 @@ mkdir -p build/windows-x86_64
 pushd build/windows-x86_64
 
 echo "[*] Building ctags"
-7z x ../../tars/$CTAGS_TAR
+7z x ../../tars/$CTAGS_TAR -so | 7z x -si -ttar 
 pushd $CTAGS_NAME
 ./autogen.sh
 ./configure --prefix=$TARGET_DIR --disable-external-sort --enable-static
